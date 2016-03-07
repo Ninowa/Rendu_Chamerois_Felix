f469d72c4ded2fe71dd4bf85620ccbdcc0ae1903
Chamerois Félix <brulelame@hotmail.fr>
2016-03-07 23:13:51 +0100

    push rendu

---
+++ New Unity Project/Assets/rotatestart.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+var choices:int[] = [ 0,90, 180, 270];
+var howManyThings:int = choices.length;
+function Start () {
+
+    var myRandomIndex1:int;
+    myRandomIndex1 = Random.Range( 0, howManyThings );
+
+
+    var result1:int;
+    result1 = choices[ myRandomIndex1 ];
+    
+    var myRandomIndex2:int;
+    myRandomIndex2 = Random.Range( 0, howManyThings );
+
+
+    var result2:int;
+    result2 = choices[ myRandomIndex2 ];
+    
+    var myRandomIndex3:int;
+    myRandomIndex3 = Random.Range( 0, howManyThings );
+
+
+    var result3:int;
+    result3 = choices[ myRandomIndex3 ];
+
+    transform.rotation= Quaternion.Euler(result1,result2,result3);
+
+}
+
---
+++ New Unity Project/Assets/scrollterrain.js
@@ -0,0 +1,12 @@
+﻿var terrain:Terrain;
+private  var splatPrototypes : SplatPrototype[];
+function Start () {
+    splatPrototypes = terrain.terrainData.splatPrototypes;
+}
+ 
+function Update () {
+ 
+    splatPrototypes[0].tileOffset.y+=0.5;
+    //terrain.terrainData.splatPrototypes[2].tileOffset.x+=0.1;
+    terrain.terrainData.splatPrototypes=splatPrototypes;
+}
